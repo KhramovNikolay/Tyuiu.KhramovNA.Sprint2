5c999c1635c4a8a8935d8103b96d8e73bd7e7da1
Николай Храмов <Tyuiu.KhramovN@outlook.com>
2023-10-15 15:25:02 +0500

    Task1

---
+++ Tyuiu.KhramovNA.Sprint2.Task1.V6.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.KhramovNA.Sprint2.Task1.V6.Lib
+{
+    public class DataService : ISprint2Task1V6
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] = (a < b) | (c < d);
+            res[1] = (a < b) & (c > d);
+            res[2] = (c > d) || (a > d);
+            res[3] = (a < b) && (c > d);
+            res[4] = !(a < b);
+            res[5] = (a > b) ^ (c > d);
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KhramovNA.Sprint2.Task1.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KhramovNA.Sprint2.Task1.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KhramovNA.Sprint2.Task1.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e69ec2c7-cd66-4e7a-ac77-930e2280c1e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
