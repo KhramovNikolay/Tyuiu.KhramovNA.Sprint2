d9b08190948fdb594d31c78ed717ba5527da3a1e
Николай Храмов <Tyuiu.KhramovN@outlook.com>
2023-10-15 15:30:17 +0500

    Task2

---
+++ Tyuiu.KhramovNA.Sprint2.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KhramovNA.Sprint2.Task2.V19.Lib;
+
+namespace Tyuiu.KhramovNA.Sprint2.Task2.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckedDotInShadeArea()
+        {
+            DataService ds = new DataService();
+            int x = 13;
+            int y = 6;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
